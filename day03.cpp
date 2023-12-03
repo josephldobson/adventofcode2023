b2609d5c7f3f7fc82df484721000318c2c46f5b8
Joseph Dobson <75431083+josephldobson@users.noreply.github.com>
2023-12-03 13:38:15 +0000

    day03 half done
    struggled with this one,

--- day03.cpp
+++ day03.cpp
@@ -1,41 +1,14 @@
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include <sstream>
-
-
-// bool isSymbol(char ch) {
-//     return ch != '.' && !isdigit(ch);
-// }
-
-
-// void partOne() {
-//     std::ifstream file("data/day01_data.txt");
-//     std::string data;
-
-//     if (file) {
-//         std::stringstream buffer;
-//         buffer << file.rdbuf();
-//         data = buffer.str();
-//     }
-
-//     std::istringstream iss(data);
-//     std::string line;
-
-//     while (std::getline(iss, line)) {
-
-//     }
-// }
 #include <iostream>
-#include <vector>
-#include <string>
+#include <fstream>
 #include <sstream>
-
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 bool isSymbol(char ch) {
     return ch != '.' && !isdigit(ch);
 }
-
 
 bool isAdjacentToSymbol(const std::vector<std::string>& schematic, int x, int y) {
     for (int i = -1; i <= 1; ++i) {
@@ -52,28 +25,57 @@ bool isAdjacentToSymbol(const std::vector<std::string>& schematic, int x, int y)
     return false;
 }
 
+void partOne() {
+    std::ifstream file("data/day03_data.txt");
+    std::string data;
 
-int main() {
-    std::vector<std::string> schematic;
+    if (file) {
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        data = buffer.str();
+    }
+
+    std::istringstream iss(data);
     std::string line;
+    std::vector<std::string> schematic;
 
-    // Read input until an empty line is encountered
-    while (std::getline(std::cin, line) && !line.empty()) {
+    while (std::getline(iss, line)) {
         schematic.push_back(line);
     }
 
+    std::string number;
+    char curr_char;
     int sum = 0;
     for (int i = 0; i < schematic.size(); ++i) {
-        std::stringstream ss(schematic[i]);
-        int number;
-        while (ss >> number) {
-            if (isAdjacentToSymbol(schematic, i, ss.tellg() - std::to_string(number).length())) {
-                sum += number;
+        for (int j = 0; j < schematic[i].size(); ++j) {
+            curr_char = schematic[i][j];
+            
+            if (isdigit(curr_char) ) {
+                number = number + curr_char;
+                if (isAdjacentToSymbol(schematic, i, j)) {
+                    j += 1;
+                    if (j < schematic[i].size()) {
+                        curr_char = schematic[i][j];
+                        while (isdigit(curr_char) && j < schematic[i].size()) {
+                            number = number + curr_char;
+                            j += 1;
+                            curr_char = schematic[i][j];
+                        }
+                        std::cout << number << std::endl;
+                        sum += stoi(number);
+                        number = "";
+                    }
+
+                }
+            } else {
+                number = "";
             }
         }
     }
 
     std::cout << "Sum of all part numbers: " << sum << std::endl;
+}
 
-    return 0;
+int main() {
+    partOne();
 }
